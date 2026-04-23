393fe761f455c22d9611c05c8f9204b3e4eb605c
Wellington <welltecnic@hotmail.com>
2026-04-23 16:46:38 -0300

    feat: codificando repositorio e interface

---
+++ LojaEcomerce/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using LojaEcomerce.Models;
+using System.Runtime.CompilerServices;
+
+namespace LojaEcomerce.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        //A INTERFACE NÃO CONTÉM CONTEM CÓDIGO APENAS A PROMESSA DO QUE DEVE
+        // SER FEITO ( COMO UM CONTRATO)
+        LoginViewModel Validar(string email, string senha);
+    }
+}
